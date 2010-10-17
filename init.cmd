edce7b3a06ccad72f1c04c910a67f56671a71f90
tenshi <sairi.na.tenshi@gmail.com>
2010-10-17 20:36:48 +0400

    master

--- init.cmd
+++ init.cmd
@@ -1,1 +1,1 @@
-git init && git pull git://github.com/sairi-na-tenshi/wc_base.git && git submodule init && git submodule update
+git init && git pull git://github.com/sairi-na-tenshi/wc_base.git master && git submodule init && git submodule update
